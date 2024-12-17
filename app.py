ea64a88a68dfb1affc8512d58f87ddafba394ca2
RaygekFox <119606829+RaygekFox@users.noreply.github.com>
2024-12-17 17:17:48 +0100

    port added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    # Run the application on 0.0.0.0 to allow external access
+    app.run(host="0.0.0.0", port=port)
